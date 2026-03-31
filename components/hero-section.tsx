62a1b04dd284e6f3628b659b2c3c192a9afaa0cd
Henrique <henriquewt.dev@gmail.com>
2026-03-31 00:21:57 -0300

    alterações finais no modelo da hero-section

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -3,19 +3,19 @@ import logo from "@/assets/logo-nobg.png";
 
 export function Hero_Section() {
     return (
-        <section className="bg-[#0b0b0b] min-h-screen flex items-center justify-center px-6 py-20 md:py-0">
-            <div className="flex flex-col md:flex-row w-full max-w-6xl">
+        <section className="select-none bg-[#0b0b0b] min-h-screen flex items-center justify-center px-6 py-20 md:py-0">
+            <div className="flex flex-col-reverse md:flex-row w-full max-w-6xl">
 
                 {/* Lado esquerdo */}
                 <div className="flex-1">
                     <div className="p-10 sm:p-14 md:p-20 h-full flex flex-col justify-center gap-8">
 
                         <h1 className="text-white text-5xl sm:text-6xl md:text-7xl font-semibold leading-tight tracking-tight">
-                            #VemSerGigante
+                            #VemSer<br className="md:hidden" />Gigante
                         </h1>
 
                         <p className="text-neutral-400 text-sm sm:text-base leading-relaxed w-full">
-                            A <span className="text-yellow-400 font-medium">IME - Jr</span> é a empresa júnior do Instituto de Matemática,
+                            A <span className="text-yellow-400 font-medium">IME Jr</span> é a empresa júnior do Instituto de Matemática,
                             Estatística e Ciência da Computação da Universidade de São Paulo (USP).
                             Formada por estudantes comprometidos com inovação e impacto, oferecemos
                             soluções nas áreas de{" "}
