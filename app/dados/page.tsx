4dc74da29e7e7ded107d058fe2e6fefafdfe440c
ioolliver <101019491+ioolliver@users.noreply.github.com>
2026-03-31 15:11:52 -0300

    feat(ui): add dados page component

---
+++ app/dados/page.tsx
@@ -0,0 +1,12 @@
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header"
+
+export default function Dados() {
+  return (
+    <main>
+      <Header />
+      <h1>Dados</h1>
+      <Footer />
+    </main>
+  );
+}
