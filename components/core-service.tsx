308804d317e8f3d43cd29f502b94ab513365f74f
lisusesc <lucasligosmail@gmail.com>
2026-04-01 13:35:25 -0300

    feat(services): enhance CoreService component with serviceNumber prop and update ServiceSection integration

--- components/core-service.tsx
+++ components/core-service.tsx
@@ -1,10 +1,11 @@
-interface IServicesSectionProps {
+interface ICoreServicesProps {
+    serviceNumber: number;
     title: string;
     color: string;
     text: string;
 }
 
-export function CoreService({ title, color, text }: IServicesSectionProps) {
+export function CoreService({ serviceNumber, title, color, text }: ICoreServicesProps) {
     return (
         <div className="flex flex-col w-full max-w-5xl">
             <h2 className="text-3xl leading-tight tracking-tight">{title.toUpperCase()}</h2>
@@ -21,14 +22,21 @@ export function CoreService({ title, color, text }: IServicesSectionProps) {
 
                 <div />
 
-                <div className="flex gap-4">
-                    <button className="py-2 px-4 cursor-pointer w-fit text-sm active:scale-95 transition-all duration-150 hover:brightness-125"
-                        style={{ backgroundColor: color }} >
-                        Contato
-                    </button>
-                    <button className="border-1 px-4 cursor-pointer w-fit text-sm active:scale-95 transition-all duration-150 hover:border-gray-400">
-                        Confira nossos trabalhos
-                    </button>
+                <div className="flex justify-between">
+                    <div className="flex gap-4">
+                        <button className="py-2 px-4 cursor-pointer w-fit text-sm active:scale-95 transition-all duration-150 hover:brightness-125"
+                            style={{ backgroundColor: color }} >
+                            Contato
+                        </button>
+                        <button className="border-1 px-4 cursor-pointer w-fit text-sm active:scale-95 transition-all duration-150 hover:border-gray-400">
+                            Confira nossos trabalhos
+                        </button>
+                    </div>
+                    <div className="flex items-center gap-2 text-[0.4em]">
+                        <div className={`bg-[#5E5E5E] p-1 rounded-4xl h-3 w-3 flex items-center justify-center ${serviceNumber === 1 ? 'opacity-100' : 'opacity-50'}`}>1</div>
+                        <div className={`bg-[#5E5E5E] p-1 rounded-4xl h-3 w-3 flex items-center justify-center ${serviceNumber === 2 ? 'opacity-100' : 'opacity-50'}`}>2</div>
+                        <div className={`bg-[#5E5E5E] p-1 rounded-4xl h-3 w-3 flex items-center justify-center ${serviceNumber === 3 ? 'opacity-100' : 'opacity-50'}`}>3</div>
+                    </div>
                 </div>
             </div>
         </div>
